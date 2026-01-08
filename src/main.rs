a3ba5ee6e17c3e2ce93f4cfed1ec67312284d379
pneumatick <jhwwefan@gmail.com>
2026-01-08 11:00:12 -0500

    Implement DNS proxy server functionality

--- src/main.rs
+++ src/main.rs
@@ -717,10 +717,8 @@ impl DnsPacket {
     }
 }
 
-fn main() -> Result<()> {
-    let qname = "yahoo.com";
-    let qtype = QueryType::MX;
-
+// Perform a DNS query by proxy
+fn lookup(qname: &str, qtype: QueryType) -> Result<DnsPacket> {
     // Google's DNS server
     let server = ("8.8.8.8", 53);
 
@@ -744,21 +742,74 @@ fn main() -> Result<()> {
     // Prepare for and receive the response in a new buffer
     let mut res_buffer = BytePacketBuffer::new();
     socket.recv_from(&mut res_buffer.buf)?;
-    let res_packet = DnsPacket::from_buffer(&mut res_buffer)?;
-    println!("{:#?}", res_packet.header);
 
-    for q in res_packet.questions {
-        println!("{:#?}", q);
+    // Return the DnsPacket
+    DnsPacket::from_buffer(&mut res_buffer)
+}
+
+// Handle an incoming packet
+fn handle_query(socket: &UdpSocket) -> Result<()> {
+    // Read the packet
+    let mut req_buffer = BytePacketBuffer::new();
+    let (_, src) = socket.recv_from(&mut req_buffer.buf)?;
+    let mut request = DnsPacket::from_buffer(&mut req_buffer)?;
+
+    // Initialize the response packet
+    let mut packet = DnsPacket::new();
+    packet.header.id = request.header.id;
+    packet.header.recursion_desired = true;
+    packet.header.recursion_available = true;
+    packet.header.response = true;
+
+    // Handle exactly one question
+    if let Some(question) = request.questions.pop() {
+        println!("Received query: {:?}", question);
+
+        // Forward the query to the target server (8.8.8.8)
+        if let Ok(result) = lookup(&question.name, question.qtype) {
+            packet.questions.push(question);
+            packet.header.rescode = result.header.rescode;
+            
+            for rec in result.answers {
+                println!("Answer: {:?}", rec);
+                packet.answers.push(rec);
+            }
+            for rec in result.authorities {
+                println!("Authority: {:?}", rec);
+                packet.authorities.push(rec);
+            }
+            for rec in result.resources {
+                println!("Resource: {:?}", rec);
+                packet.resources.push(rec);
+            }
+        }
+        else {
+            packet.header.rescode = ResultCode::SERVFAIL;
+        }
     }
-    for rec in res_packet.answers {
-        println!("{:#?}", rec);
+    else {
+        packet.header.rescode = ResultCode::FORMERR;
     }
-    for rec in res_packet.authorities {
-        println!("{:#?}", rec);
-    }
-    for rec in res_packet.resources {
-        println!("{:#?}", rec);
-    }
+
+    // Encode and send response
+    let mut res_buffer = BytePacketBuffer::new();
+    packet.write(&mut res_buffer)?;
+
+    let len = res_buffer.pos();
+    let data = res_buffer.get_range(0, len)?;
+    socket.send_to(data, src)?;
 
     Ok(())
+}
+
+fn main() -> Result<()> {
+    let socket = UdpSocket::bind(("0.0.0.0", 2053))?;
+
+    // Loop to perpetually handle requests
+    loop {
+        match handle_query(&socket) {
+            Ok(_) => {},
+            Err(e) => eprintln!("An error occurred: {}", e),
+        }
+    }
 }
